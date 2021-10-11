94352b81965368f41175fcfe1b282f93c4c2ef5f
Alejandro Babio <alejandroebabio@gmail.com>
2021-10-11 13:43:45 -0300

    Scaffolds Posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  constraints(subdomain: Constraints::Subdomain.new.to_regexp) do
+    resources :posts
+  end
 end
